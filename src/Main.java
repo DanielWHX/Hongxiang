79e61ba2147ea7b117211d73ff1172ac0a7fa8aa
DanielWHX <129551786+DanielWHX@users.noreply.github.com>
2025-03-08 12:30:56 -0800

    interface done

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,24 @@
+import models.TCGItem;
+import models.User;
+import services.TCGMarketplaceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        TCGMarketplace marketplace = new TCGMarketplace();
+        TCGMarketplaceImpl marketplace = new TCGMarketplaceImpl();
 
-        // Create items
+        // create items
         TCGItem item1 = new TCGItem("001", "Umbreon ex", 340);
         TCGItem item2 = new TCGItem("002", "Sylveon ex", 127);
 
-        // Create user
+        // create user
         User buyer = new User("14953", "Daniel");
 
-        // List items for sale
+        // upload items for sale
         marketplace.listItemForSale(item1);
         marketplace.listItemForSale(item2);
 
-        // Check if an item is available
-        System.out.println("Is Umbreon ex available? "
-                + marketplace.isItemAvailable("002"));
-
-        // Buy an item
+        // buy items
+        marketplace.buyItem("001", buyer);
         marketplace.buyItem("002", buyer);
-        System.out.println("Is Umbreon ex available after purchase? "
-                + marketplace.isItemAvailable("002"));
-
-        // Simulate market price updates
-        marketplace.updateMarketPrices();
     }
 }
