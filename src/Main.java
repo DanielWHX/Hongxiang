6e1ad12be42811a11078c9c537971ceac724d4c3
DanielWHX <129551786+DanielWHX@users.noreply.github.com>
2025-04-16 16:20:23 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,5 @@
-import components.tcg.customer.User;
-import components.tcg.marketplace.TCGMarketplace1;
-import components.tcg.stock.TCGItem;
-
 public class Main {
         public static void main(String[] args) {
-                TCGMarketplace1 marketplace = new TCGMarketplace1();
 
-                // Add categories
-                marketplace.addCategory("Booster Box");
-                marketplace.addCategory("Psychic Set");
-
-                // Add items
-                TCGItem item1 = new TCGItem("001", "Charizard EX", 300.0, 3,
-                                "Booster Box");
-                TCGItem item2 = new TCGItem("002", "Pikachu VMAX", 150.0, 2,
-                                "Booster Box");
-                TCGItem item3 = new TCGItem("003", "Sylveon GX", 120.0, 5,
-                                "Psychic Set");
-
-                marketplace.listItemForSale(item1);
-                marketplace.listItemForSale(item2);
-                marketplace.listItemForSale(item3);
-
-                marketplace.assignItemToCategory("001", "Booster Box");
-                marketplace.assignItemToCategory("002", "Booster Box");
-                marketplace.assignItemToCategory("003", "Psychic Set");
-
-                // Add a user.
-                User user = new User("U001", "Daniel");
-
-                // Buy an item.
-                System.out.println("Buying item 001...");
-                marketplace.buyItem("001", user);
-
-                // Print results.
-                System.out.println("Available? "
-                                + marketplace.isItemAvailable("001"));
-                System.out.println("Quantity left: "
-                                + marketplace.getQuantity("001"));
-                System.out.println("Items in 'Booster Box': " + marketplace
-                                .getItemsByCategory("Booster Box"));
-                System.out.println("All available items: "
-                                + marketplace.getAllItemsForSale());
         }
 }
