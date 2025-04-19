992c96a4f389ed591343eabc2e647eaece49aa45
DanielWHX <129551786+DanielWHX@users.noreply.github.com>
2025-04-18 20:02:59 -0400

    1

--- src/components/tcg/marketplace/Listing.java
+++ src/components/tcg/marketplace/Listing.java
@@ -1,8 +1,8 @@
-package components.tcg.marketplace;
+// package components.tcg.marketplace;
 
-import components.tcg.stock.TCGItem;
+// import components.tcg.stock.TCGItem;
 
-public record Listing(TCGItem item, double price, int quantity,
-        String category) {
+// public record Listing(TCGItem item, double price, int quanti
+                   String category) {
 
-}
+// }
